8e85043e9df9a6e8d955c4db3a926429fdc5f3ca
DAP <dlrayjr89@gmail.com>
2020-06-17 18:34:39 +0000

    project completed MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,39 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
+
+'''
+binarySearch(arr, target, first, last)
+        if first > last
+            i want to return false
+        else 
+            set mid point = first + last / 2
+                if target is equal to arr[midpoint]
+                    return midpoint
+                else if target < data[mid]
+                    return binarySearch(arr, target, mid + 1, last)
+                else
+                return binarySearch(arr, target,last, mid - 1)
+
+'''
+def binary_search(arr, target, first, last):
     # Your code here
+    if last >= first:
+        midpoint = first + (last - first) // 2
+
+        #if found at mid point then return
+        if arr[midpoint] == target:
+            return midpoint
+        
+        # search through the left
+        elif arr[midpoint] > target:
+            return binary_search(arr, target, first, midpoint - 1)
+        # search through the right
+        else:
+            return binary_search(arr, target, midpoint + 1, last)
+
+    else:
+         return -1
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +42,7 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
+#     pass
     # Your code here
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,16 +1,64 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(arr, arrA, midArr, arrB):
+    pass
+    # leftArr = midArr - arrA + 1
+    # rightArr = arrB - midArr
+
+    # for i in range(len(leftArr)):
+    #     leftArr[i] = arr[arrA + i]
+    # for j in range(len(rightArr)):
+    #     rightArr[j] = arr[midArr + 1, j]
+
+    #     while i < leftArr and rightArr:
+    #         if leftArr[i] <= rightArr[j]: 
+    #             return arr 
+
 
     # Your code here
 
 
-    return merged_arr
+    # return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        #creat a  main is point where this arr is divided into subarrays
+        mainArr = len(arr) / 2
+        # set up left side subarr1
+        LeftArr = arr[:int(mainArr)]
+        # set up right side subarr2
+        righArr = arr[int(mainArr):]
+
+        ## recursively divide and sort the two halves
+        merge_sort(LeftArr)
+        merge_sort(righArr)
+
+        #set idx with = left
+        #set j with right
+        #k is set with the arr
+        idx = j = k = 0
+
+        #When i reach the end of both left and right pick greater element an dput them in corrent position
+        while idx < len(LeftArr) and j < len(righArr):
+            if LeftArr[idx] < righArr[j]:
+                arr[k] = LeftArr[idx]
+                idx += 1
+            else:
+                arr[k + 1] = righArr[j + 1]
+            #     j += 1
+            # k += 1
+
+
+        # once elements run out in either left or right . grab the remaining elements and put them in arr
+        while idx < len(LeftArr):
+            arr[k + 1] = LeftArr[idx + 1]
+            # idx += 1
+            # k += 1
+        while j < len(righArr):
+            arr[k + 1] = righArr[j + 1]
+            # j += 1
+            # k += 1
 
 
     return arr
@@ -20,9 +68,11 @@ def merge_sort(arr):
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
+    pass
     # Your code here
 
 
 def merge_sort_in_place(arr, l, r):
+    pass
     # Your code here
 
